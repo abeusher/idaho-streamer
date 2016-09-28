cdf2b7bbd983422e314504811b4778403fc8716f
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-09-28 15:17:19 -0500

    removing direct tile_dump with git lfs version

--- idaho_streamer/db.py
+++ idaho_streamer/db.py
@@ -10,28 +10,12 @@ use_ssl=False
 assert "MONGO_CONNECTION_STRING" in os.environ
 mongo_url = os.environ.get("MONGO_CONNECTION_STRING")
 
-
-#put something in a database
-
-from itertools import cycle
-
-fake_post = {"fromDate": "Tue Aug 06 02:23:21 +0000 2013", "toDate": ""}
-d1 = {"zxy": [12, 2632, 1616], "url": "http://bullshit.com", "idahoID": "cd1adcb2-84ca-45da-8185-5a3bb8e34b2b",
-"bounds": [51.328125, 35.389, 51.416, 35.460], "center": [51.37, 35.424], "created_at": "Tue Aug 06 02:23:21 +0000 2013"}
-d2 = d1.copy()
-d2['created_at'] = "Wed Aug 07 02:23:21 +0000 2013"
-d3 = d1.copy()
-d3['created_at'] = "Fri Aug 09 02:23:21 +0000 2013"
-fake_data = [d1, d2, d3]
-fake_cycle = cycle(fake_data)
-
-
-connection = None
 if use_ssl:
     connection = ConnectionPool(mongo_url, ssl_context_factory=ssl.ClientContextFactory())
 else:
     connection = ConnectionPool(mongo_url)
 db = connection.get_default_database()
+
 
 @inlineCallbacks
 def create_collections(db):
@@ -43,6 +27,7 @@ def create_collections(db):
             created.append(key)
     log.msg("Created collections: {}".format(",".join(created)))
     returnValue(created)
+
 
 @inlineCallbacks
 def drop_collections(db):
@@ -56,9 +41,11 @@ def drop_collections(db):
     log.msg("Dropped collections: {}".format(",".join(dropped)))
     returnValue(dropped)
 
+
 @inlineCallbacks
 def populate(db):
     yield db.idaho_tiles.insert_many(fake_data)
+
 
 @inlineCallbacks
 def init():
