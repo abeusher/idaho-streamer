6fd9ac4b674b731fdb093024294b2230689c8679
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-09-28 16:05:42 -0500

    initializing with dumped fixture data

--- idaho_streamer/db.py
+++ idaho_streamer/db.py
@@ -1,14 +1,23 @@
 import os
+import os.path
 from twisted.python import log
 from twisted.internet import ssl
 from twisted.internet.defer import inlineCallbacks, returnValue
 from txmongo.connection import ConnectionPool
+from txmongo import filter as qf
 
 COLLECTIONS = ["idaho_tiles"]
 
 use_ssl=False
 assert "MONGO_CONNECTION_STRING" in os.environ
 mongo_url = os.environ.get("MONGO_CONNECTION_STRING")
+
+
+from bson.json_util import loads
+fixture_file = os.path.join(os.path.dirname(__file__), "..", "test", "fixtures", "tile_dump.json")
+
+with open(fixture_file) as f:
+    fixture_data = loads(f.read())
 
 if use_ssl:
     connection = ConnectionPool(mongo_url, ssl_context_factory=ssl.ClientContextFactory())
@@ -25,6 +34,8 @@ def create_collections(db):
         if key not in current_collections:
             yield db.create_collection(key)
             created.append(key)
+    # idaho_tiles index
+    db.idaho_tiles.create_index(qf.sort(qf.ASCENDING("_acquisitionDate")))
     log.msg("Created collections: {}".format(",".join(created)))
     returnValue(created)
 
@@ -44,7 +55,7 @@ def drop_collections(db):
 
 @inlineCallbacks
 def populate(db):
-    yield db.idaho_tiles.insert_many(fake_data)
+    yield db.idaho_tiles.insert_many(fixture_data)
 
 
 @inlineCallbacks
