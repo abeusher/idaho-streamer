3c955460bab469b08c4556700a487ec2a752acdd
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-09-28 19:07:38 -0500

    actual streaming

--- idaho_streamer/main.py
+++ idaho_streamer/main.py
@@ -2,8 +2,10 @@ import datetime as dt
 import json
 from bson.json_util import dumps as json_dumps
 from klein import Klein
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.task import LoopingCall
 from twisted.python import log
+from txmongo import filter as qf
 from shapely.geometry import shape, Polygon
 from dateutil.parser import parse as parse_date
 
@@ -13,6 +15,7 @@ from idaho_streamer.db import db
 
 app = Klein()
 MAX_POST_BODY = 1024*1024 # 1MB
+POLL_INTERVAL = 15.0 # seconds
 
 def parse_json_body(content):
     try:
@@ -57,6 +60,7 @@ def not_acceptable(request, failure):
 
 
 @app.route("/filter", methods=["POST"])
+@inlineCallbacks
 def filter_post(request):
     params = parse_json_body(request.content.read())
     fromDate = parse_date(params.get("fromDate", (dt.datetime.now() - dt.timedelta(weeks=1)).isoformat()))
@@ -70,16 +74,32 @@ def filter_post(request):
         raise BadRequest("Delay should be a floating point number")
     request.setHeader('Content-Type', 'application/json')
     request.setResponseCode(200)
-    return stream_data(request, fromDate, toDate, bbox, delay)
-
+    last_id = yield backfill(request, fromDate, toDate, bbox, delay)
+    yield stream(request, last_id, bbox, delay)
 
 @inlineCallbacks
-def stream_data(request, fromDate, toDate, bbox, delay):
+def backfill(request, fromDate, toDate, bbox, delay):
+    last_rec = yield db.idaho_tiles.find({}, fields={"_id": True}, limit=1, filter=qf.sort(qf.DESCENDING("_id")))
+    last_id = last_rec[0]["_id"]
     docs, d  = yield db.idaho_tiles.find({"_acquisitionDate": {"$gte": fromDate, "$lt": toDate}},
-                                         fields={"_id": False, "_acquisitionDate": False}, cursor=True)
+                                         fields={"_acquisitionDate": False}, cursor=True)
     while docs:
         for doc in docs:
+            last_id = doc["_id"]
             if bbox is None or bbox.intersects(shape(doc["geometry"])):
                 request.write(json_dumps(doc))
                 yield sleep(delay)
         docs, d = yield d
+    returnValue(last_id)
+
+@inlineCallbacks
+def stream(request, from_id, bbox, delay):
+    while True:
+        ref = dt.datetime.now()
+        docs = yield db.idaho_tiles.find({"_id": {"$gt": from_id}})
+        for doc in docs:
+            if bbox is None or bbox.intersects(shape(doc["geometry"])):
+                request.write(json_dumps(doc))
+                yield sleep(delay)
+        delta = (dt.datetime.now() - ref).total_seconds()
+        yield sleep(max(0.0, POLL_INTERVAL - delta))
