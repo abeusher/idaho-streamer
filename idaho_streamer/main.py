b5353706bb21bc05b7a743cb58786b1b502601ee
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-10-04 10:47:30 -0500

    sorting results by acquisitionDate in ascending order

--- idaho_streamer/main.py
+++ idaho_streamer/main.py
@@ -1,9 +1,11 @@
+import os.path
 import datetime as dt
 import json
 from bson.json_util import dumps as json_dumps
 from klein import Klein
 from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.internet.task import LoopingCall
+from twisted.web.static import File
 from twisted.python import log
 from txmongo import filter as qf
 from shapely.geometry import shape, Polygon
@@ -17,29 +19,6 @@ app = Klein()
 MAX_POST_BODY = 1024*1024 # 1MB
 POLL_INTERVAL = 15.0 # seconds
 
-def parse_json_body(content):
-    try:
-        assert len(content) < MAX_POST_BODY
-        result = json.loads(content)
-    except AssertionError:
-        raise NotAcceptable("Content length: {} too large".format(len(content)))
-    except ValueError:
-        raise BadRequest("Malformed JSON in request body")
-    return result
-
-def parse_bbox(bbox):
-    bbox = bbox.split(",")
-    try:
-        assert len(bbox) == 4
-        bbox = [float(v) for v in bbox]
-        bbox = Polygon([[bbox[0], bbox[1]],
-                        [bbox[0], bbox[3]],
-                        [bbox[2], bbox[3]],
-                        [bbox[2], bbox[1]],
-                        [bbox[0], bbox[1]]])
-    except AssertionError, ValueError:
-        raise BadRequest("Expected 4 comma separated numbers.  Got {}".format(bbox))
-    return bbox
 
 @app.handle_errors(BadRequest)
 def bad_request(request, failure):
@@ -57,6 +36,12 @@ def not_found(request, failure):
 def not_acceptable(request, failure):
     request.setResponseCode(406)
     return failure.getErrorMessage()
+
+
+@app.route("/", branch=True)
+def index(request):
+    # TODO: disable directory listing
+    return File(os.path.join(os.path.dirname(__file__), "public"))
 
 
 @app.route("/filter", methods=["POST"])
@@ -83,12 +68,40 @@ def filter_post(request):
     if enable_streaming:
         yield stream(request, last_id, bbox, delay)
 
+
+def parse_json_body(content):
+    try:
+        assert len(content) < MAX_POST_BODY
+        result = json.loads(content)
+    except AssertionError:
+        raise NotAcceptable("Content length: {} too large".format(len(content)))
+    except ValueError:
+        raise BadRequest("Malformed JSON in request body")
+    return result
+
+
+def parse_bbox(bbox):
+    bbox = bbox.split(",")
+    try:
+        assert len(bbox) == 4
+        bbox = [float(v) for v in bbox]
+        bbox = Polygon([[bbox[0], bbox[1]],
+                        [bbox[0], bbox[3]],
+                        [bbox[2], bbox[3]],
+                        [bbox[2], bbox[1]],
+                        [bbox[0], bbox[1]]])
+    except AssertionError, ValueError:
+        raise BadRequest("Expected 4 comma separated numbers.  Got {}".format(bbox))
+    return bbox
+
+
 @inlineCallbacks
 def backfill(request, fromDate, toDate, bbox, delay):
     last_rec = yield db.idaho_tiles.find({}, fields={"_id": True}, limit=1, filter=qf.sort(qf.DESCENDING("_id")))
     last_id = last_rec[0]["_id"]
     docs, d  = yield db.idaho_tiles.find({"_acquisitionDate": {"$gte": fromDate, "$lt": toDate}},
-                                         fields={"_acquisitionDate": False}, cursor=True)
+                                         fields={"_acquisitionDate": False}, cursor=True,
+                                         filter=qf.sort(qf.ASCENDING("_acquisitionDate")))
     while docs:
         for doc in docs:
             last_id = doc["_id"]
@@ -99,11 +112,12 @@ def backfill(request, fromDate, toDate, bbox, delay):
         docs, d = yield d
     returnValue(last_id)
 
+
 @inlineCallbacks
 def stream(request, from_id, bbox, delay):
     while True:
         ref = dt.datetime.now()
-        docs = yield db.idaho_tiles.find({"_id": {"$gt": from_id}})
+        docs = yield db.idaho_tiles.find({"_id": {"$gt": from_id}}, filter=qf.sort(qf.ASCENDING("_acquisitionDate")))
         for doc in docs:
             if bbox is None or bbox.intersects(shape(doc["geometry"])):
                 request.write(json_dumps(doc))
