75a5a5f844020dccd050cd80f34f28aab83f073a
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-10-14 00:23:20 -0500

    support for the toa vrt endpoint

--- idaho_streamer/aws.py
+++ idaho_streamer/aws.py
@@ -4,6 +4,13 @@ from boto import connect_sqs
 from boto.dynamodb2.table import Table
 from boto.dynamodb2 import connect_to_region
 from twisted.internet.threads import deferToThread
+from twisted.internet.defer import inlineCallbacks, returnValue
+from itertools import product
+import hashlib
+import json
+import xml.etree.ElementTree as ET
+
+from idaho_streamer.util import calc_toa_gain_offset
 
 _dbconn = connect_to_region("us-east-1", profile_name="dg")
 _images = Table('IDAHOIngestedImages', connection=_dbconn)
@@ -12,9 +19,11 @@ _bucket = _s3conn.get_bucket('idaho-images')
 _sqsconn = connect_sqs(profile_name="dg")
 _queue = _sqsconn.get_queue('timbr-idaho-streaming')
 
+
 def get_idaho_metadata(idaho_id, files=['IMD','TIL']):
     prefix = '{}/vendor-metadata'.format(idaho_id)
     return { f: _bucket.get_key('{}/{}.metadata'.format(prefix, f)).get_contents_as_string() for f in files}
+
 
 def iterimages(limit=10):
     for rec in _images.scan(limit=limit):
@@ -24,6 +33,47 @@ def iterimages(limit=10):
 def next_batch():
     return _queue.get_messages(10)
 
+
 def remove_batch(batch):
     for msg in batch:
         _queue.delete_message(msg)
+
+
+def vrt_for_id(idaho_id, meta):
+    md = json.loads(_bucket.get_key('{}/image.json'.format(idaho_id)).get_contents_as_string())
+    warp = json.loads(_bucket.get_key('{}/native_warp_spec.json'.format(idaho_id)).get_contents_as_string())
+    gains_offsets = calc_toa_gain_offset(meta)
+    tfm = warp["targetGeoTransform"]
+    vrt = ET.Element("VRTDataset", {"rasterXSize": str(md["tileXSize"]*md["numXTiles"]),
+                                "rasterYSize": str(md["tileYSize"]*md["numYTiles"])})
+    ET.SubElement(vrt, "SRS").text = tfm["spatialReferenceSystemCode"]
+    # NOTE: shearX and shearY may work by coincidence or they may be incorrect terms
+    ET.SubElement(vrt, "GeoTransform").text = ", ".join(map(str, [tfm["translateX"],
+                                                                  tfm["scaleX"],
+                                                                  tfm["shearX"],
+                                                                  tfm["translateY"],
+                                                                  tfm["shearY"],
+                                                                  tfm["scaleY"]]))
+
+    paths = []
+    for i in xrange(md["numBands"]):
+        bidx = i+1
+        band = ET.SubElement(vrt, "VRTRasterBand", {"dataType": "Float32", "band": str(bidx)})
+        for x, y in product(xrange(md["numXTiles"]), xrange(md["numYTiles"])):
+            formatted_string = "{}-{}-{}".format(idaho_id, x, y)
+            hashed_string = hashlib.md5(formatted_string).hexdigest()
+            prefix = hashed_string[:4]
+            path = "{partition}{prefix}/{idaho_id}/{x}/{y}.{fmt}".format(partition=md["tilePartition"], prefix=prefix, hashed_string=hashed_string,
+                                                                                        idaho_id=idaho_id, x=x, y=y, fmt=md["nativeTileFileFormat"].lower())
+            src = ET.SubElement(band, "ComplexSource")
+            ET.SubElement(src, "SourceFilename").text = "/vsis3/idaho-images/{}".format(path)
+            ET.SubElement(src, "SourceBand", text=str(bidx))
+            ET.SubElement(src, "SrcRect", {"xOff": str(md["tileXOffset"]), "yOff": str(md["tileYOffset"]),
+                                            "xSize": str(md["tileXSize"]), "ySize": str(md["tileYSize"])})
+            ET.SubElement(src, "DstRect", {"xOff": str(x*md["tileXSize"]), "yOff": str(y*md["tileYSize"]),
+                                            "xSize": str(md["tileXSize"]), "ySize": str(md["tileYSize"])})
+
+
+            ET.SubElement(src, "ScaleOffset").text = str(gains_offsets[i][1])
+            ET.SubElement(src, "ScaleRatio").text = str(gains_offsets[i][0])
+    returnValue(ET.tostring(vrt))
--- idaho_streamer/util.py
+++ idaho_streamer/util.py
@@ -1,11 +1,17 @@
 from twisted.internet import reactor, defer
 import pvl
 import geoio.constants as constants
+import requests
+import numpy as np
+import ephem
+from bson.json_util import loads as json_loads
+
 
 def sleep(seconds):
     d = defer.Deferred()
     reactor.callLater(seconds, d.callback, seconds)
     return d
+
 
 def extract_idaho_metadata(imd, til):
     meta = {}
@@ -54,3 +60,51 @@ def extract_idaho_metadata(imd, til):
 
 
     return meta
+
+
+def calc_toa_gain_offset(meta):
+    """
+    Compute (gain, offset) tuples for each band of the specified image metadata
+    """
+
+    # Set satellite index to look up cal factors
+    sat_index = meta['satid'].upper() + "_" + \
+                meta['bandid'].upper()
+
+    # Set scale for at sensor radiance
+    # Eq is:
+    # L = GAIN * DN * (ACF/EBW) + Offset
+    # ACF abscal factor from meta data
+    # EBW effectiveBandwidth from meta data
+    # Gain provided by abscal from const
+    # Offset provided by abscal from const
+    acf = np.asarray(meta['abscalfactor']) # Should be nbands length
+    ebw = np.asarray(meta['effbandwidth'])  # Should be nbands length
+    gain = np.asarray(constants.DG_ABSCAL_GAIN[sat_index])
+    scale = (acf/ebw)*(gain)
+    offset = np.asarray(constants.DG_ABSCAL_OFFSET[sat_index])
+
+    e_sun_index = meta['satid'].upper() + "_" + \
+                  meta['bandid'].upper()
+    e_sun = np.asarray(constants.DG_ESUN[e_sun_index])
+    sun = ephem.Sun()
+    img_obs = ephem.Observer()
+    img_obs.lon = meta['latlonhae'][1]
+    img_obs.lat = meta['latlonhae'][0]
+    img_obs.elevation = meta['latlonhae'][2]
+    img_obs.date = meta['img_datetime_obj_utc']
+    sun.compute(img_obs)
+    d_es = sun.earth_distance
+
+    ## Pull sun elevation from the image metadata
+    #theta_s can be zenith or elevation - the calc below will us either
+    # a cos or s in respectively
+    #theta_s = float(self.meta_dg.IMD.IMAGE.MEANSUNEL)
+    theta_s = 90-float(meta['mean_sun_el'])
+    scale2 = (d_es ** 2 * np.pi) / (e_sun * np.cos(np.deg2rad(theta_s)))
+
+    scale = scale * scale2
+    offset = offset * scale2
+
+    # Return scaled data
+    return zip(scale, offset)
